756d5286e2545888efb8e94cc883f74ec5f0a8b0
venkateswara Rao Atchutha <venkatesh.atchutha09@gmail.com>
2022-03-07 13:33:53 +0530

    Eager initialization drawback

---
+++ LearnDP/src/Creational/Singletondp/cleint.java
@@ -0,0 +1,17 @@
+package Creational.Singletondp;
+
+
+public class cleint {
+    public static void main(String[] args) {
+        //signleton signleton = Creational.Singletondp.signleton.getInstance();
+        //Creational.Singletondp.signleton signleton1= signleton.instance;
+        System.out.println(signleton.getShowheight());
+        System.out.println(signleton.getShowheight());
+        //signleton signleton2 = Creational.Singletondp.signleton.getInstance();
+        //System.out.println(signleton2);
+//        Thread th=new Thread();
+//        Thread th2=new Thread();
+//        th.
+
+    }
+}
---
+++ LearnDP/src/Creational/Singletondp/signleton.java
@@ -0,0 +1,26 @@
+package Creational.Singletondp;
+
+public class signleton {
+    private signleton(){
+
+    }
+    private static signleton instance=new signleton(); // Eager initialization this is thread safe but may lead to resource wastage
+    //public static signleton instance=null ;
+    private static signleton showinstace=instance;
+    private static int showheight=11;
+    public static int getShowheight(){
+        System.out.println(getShowinstace());// initializing the static variables when ever the class name is called. which is wastage of space
+        return showheight;
+    }
+
+    public static signleton getShowinstace() {
+        return showinstace;
+    }
+
+    public static signleton getInstance(){
+//        if (instance==null){
+//            instance= new signleton();
+//        }
+        return instance;
+    }
+}
